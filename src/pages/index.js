6f3106553198bd2c632365e927d8830459f2ed0d
igor-dv <davydkin.igor@gmail.com>
2018-02-27 12:58:18 +0200

    Add back ground to the index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,27 +2,14 @@ import React from 'react';
 import Greeting from '../components/Greeting';
 import LinksList from '../components/LinksList';
 
-const style = {
-  page: {
-    position: 'absolute',
-    width: '100%',
-    height: '100%',
-    overflow: 'hidden',
-  },
-  inner: {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-  },
-};
+import './index.css';
 
 const IndexPage = () => (
-  <div style={style.page}>
-    <div style={style.inner}>
+  <div className="page page-style">
+    <section className="inner inner-style">
       <Greeting />
       <LinksList />
-    </div>
+    </section>
   </div>
 );
 
