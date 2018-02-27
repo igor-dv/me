47d6d60ad94614dd2bf180183dcab5082b453c5d
igor-dv <davydkin.igor@gmail.com>
2018-02-27 16:10:45 +0200

    Make the emoji appear in delay

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styles from './Greeting.module.css';
 
 const emojis = ['🤖', '😈', '😴', '😎', '🤔', '😜', '😵', '🍻', '🎮'];
 
@@ -7,13 +8,39 @@ const randomEmoji = () => {
   return emojis[index];
 };
 
-const Greeting = () => {
-  return (
-    <h1>
-      Hi, my name is Igor
-      {randomEmoji()}
-    </h1>
-  );
-};
+class Greeting extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      emoji: randomEmoji(),
+      shown: false,
+    };
+  }
+
+  getEmojiClass() {
+    return this.state.shown
+      ? `${styles.emoji} ${styles.emojiShown}`
+      : styles.emoji;
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState(prev => ({
+        ...prev,
+        shown: true,
+      }));
+    }, 1000);
+  }
+
+  render() {
+    return (
+      <h1>
+        Hi, my name is Igor{' '}
+        <span className={this.getEmojiClass()}>{this.state.emoji}</span>
+      </h1>
+    );
+  }
+}
 
 export default Greeting;
