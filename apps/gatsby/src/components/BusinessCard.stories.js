9853e69fc2fd3b47e66313b261c8748ba2714497
igor-dv <davydkin.igor@gmail.com>
2018-03-01 14:08:01 +0200

    Patch gatsby to work with the babel-loader from the the gatsby/node-modules

---
+++ apps/gatsby/src/components/BusinessCard.stories.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import BusinessCard from './BusinessCard';
+
+storiesOf('gatsby/components/BusinessCard', module).add('simple', () => (
+  <BusinessCard />
+));
--- apps/gatsby/src/pages/_index.stories.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import Main from './';
-
-storiesOf('gatsby/pages/Main', module).add('simple', () => <Main />);
